18437ec5f1130e4747a86578ea7b25bfb39b2cff
Hafiz Muhammad Danial <danialijaz098@gmail.com>
2024-11-11 01:42:14 -0800

    component reuseable keya hn

--- App.js
+++ App.js
@@ -1,47 +1,24 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, ScrollView } from 'react-native';
+import Profile_card from './profileCard';
 
-const Profile_card = () => {
+const profile = {
+  name: 'HAFIZ MUHAMMAD DANIAL',
+  bio: 'Skilled developer with experience in React Native, Visual Studio.',
+  uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcyI9Cvp53aaP9XeRn-ZKbJDH2QaWC72O26A&s',
+};
+const profile1 = {
+  name: 'Sermad',
+  bio: 'Skilled developer with experience in React Native, Visual Studio.',
+  uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcyI9Cvp53aaP9XeRn-ZKbJDH2QaWC72O26A&s',
+};
+const App = () => {
   return (
-    <View style={style.container}>
-      <Image
-        style={style.image}
-        source={{
-          uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcyI9Cvp53aaP9XeRn-ZKbJDH2QaWC72O26A&s',
-        }}
-      />
-      <Text style={style.name}>HAFIZ MUHAMMAD DANIAL</Text>
-      <Text style={style.bio}>Skilled developer with experience in React Native, Visual Studio.</Text>
-    </View>
+    <ScrollView>
+      <Profile_card profile={profile} />
+      <Profile_card profile={profile1} />
+
+    </ScrollView>
   );
 };
-const style = StyleSheet.create({
-  container: {
-    backgroundColor: '#f9f9f9',
-    margin: 20,
-    padding: 20,
-    alignItems: 'center',
-    shadowColor: 'black',
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 80,
-  },
-  image: {
-    width: 200,
-    height: 200,
-    borderRadius: 100,
-    margin: 50,
-  },
-  name:{
-    fontSize:16,
-    fontWeight:'bold',
-    padding:10,
-    marginTop: -45,
-  },
-  bio:{
-    textAlign:'center',
-    color:'black',
-    marginTop: 5
-  }
-});
-
-export default Profile_card;
+export default App;
---
+++ profileCard.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+
+
+const Profile_card = ({profile}) => {
+  return (
+    <View style={style.container}>
+      <Image
+        style={style.image}
+        source={{
+          uri: profile.uri
+        }}
+      />
+      <Text style={style.name}>{profile.name}</Text>
+      <Text style={style.bio}>{profile.bio}</Text>
+    </View>
+  );
+};
+const style = StyleSheet.create({
+  container: {
+    backgroundColor: '#f9f9f9',
+    margin: 20,
+    padding: 20,
+    alignItems: 'center',
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 80,
+    borderRadius:20,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    margin: 50,
+  },
+  name:{
+    fontSize:16,
+    fontWeight:'bold',
+    padding:10,
+    marginTop: -45,
+  },
+  bio:{
+    textAlign:'center',
+    color:'black',
+    marginTop: 5
+  }
+});
+
+export default Profile_card;
